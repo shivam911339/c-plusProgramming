93ba5d92179896a7ea8003adfcd202d5de560ae8
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2023-07-21 22:30:20 +0530

    Add files via upload

---
+++ typecast.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	float c;
+	a=13;
+	b=2;
+	c=(float)a/b;
+	cout<<c;
+	return 0;
+}
---
+++ typedef.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+typedef int marks,section;
+int main()
+{
+	marks m1,m2;
+	cout<<"Enter marks: "<<endl;
+	cin>>m1>>m2;
+	cout<<m1<<" "<<m2;
+	return 0;
+}
