791d0c998261f54188d9c61ffe9cdc434eac3236
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2023-07-24 11:50:19 +0530

    loop

---
+++ forloop2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,n;
+	cout<<"Enter a number: ";
+	cin>>n;
+	i=1;
+	for(;;)
+	{
+		cout<<i;
+		i++;
+		if(i==10)
+			break;
+			
+	}
+}
