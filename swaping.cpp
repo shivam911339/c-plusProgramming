a9490656d1b32822c11c70ca5889ded8a96595d6
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2023-08-02 17:59:24 +0530

    Add files via upload

---
+++ swaping.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter two number: ";
+	cin>>a>>b;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"The swaping number is "<<a<<b;
+	return 0;
+	
+}
