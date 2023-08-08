f3a68273cf8ab1e016e5f235add82bd68df9aa1f
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2023-08-08 20:36:16 +0530

    Add files via upload

---
+++ maximum0finarray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int A[]={1,2,3,4,5,67,8};
+	for(int i=0;i<9;i++)
+	{
+		int max=1;
+		if(A[i]>max){
+			max=A[i];
+		}
+		
+	}
+	cout<<"The max in array is "<<max;
+	
+}
