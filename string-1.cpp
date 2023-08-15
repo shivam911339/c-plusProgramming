b073990245a3b66d5c94c329f450f68d11438470
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2023-08-15 22:22:10 +0530

    Add files via upload

---
+++ string-1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char A[]="Hello!";
+	cout<<A<<endl;
+	char B[]={'1','2','3','4','\0'};
+	cout<<B<<endl;
+	string S="Hello!";
+	cout<<S<<endl;
+	
+}
---
+++ string-2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	char s[10];
+//	cout<<"Enter the name: ";
+//	cin>>s;
+//	cout<<s<<endl;
+//}
+
+int main()
+{
+	char S[30];
+	cout<<"Enter the Name:   ";
+	cin.getline(S,30);
+	cout<<S<<endl;
+}
