ccf695ba42d7716d1270bd4bdd791e5e8bc9f472
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2023-09-04 23:25:32 +0530

    Add files via upload

---
+++ classConstructor2.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+class Rectangle
+{
+	private:
+	int lenght;
+	int breadth;
+	public:
+	int setLenght(int l)
+	{
+		if(l>=0){
+			lenght=l;
+		}
+		else
+			cout<<"Enter the valid breadth..";	
+	}
+	int setBreadth(int b)
+	{
+		if(b>=0)
+		{
+			breadth=b;	
+		}
+		else
+			cout<<"Enter valid breadth...";
+	}
+	
+	Rectangle(int l=0,int b=0)
+	{
+		setLenght(l);
+		setBreadth(b);
+	}
+	
+	Rectangle(Rectangle &rec)
+	{
+		lenght=rec.lenght;
+		breadth=rec.breadth;
+	}
+	
+	int getLenght()
+	{
+		return lenght;
+	}
+	
+	int getBreadth()
+	{
+		return breadth;
+	}
+	
+	int area()
+	{
+		return lenght*breadth;
+	}
+	int perimeter()
+	{
+		return 2*(lenght+breadth);
+	}
+		
+	
+};
+int main()
+{
+	Rectangle r1(10,5);
+	Rectangle r2(r1);
+	cout<<r2.area()<<endl;
+	
+}
+
